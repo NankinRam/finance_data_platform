15045cf9c9ae07320d75a5db8d7b499a0e786b99
NankinRam <vadimnankin12@gmail.com>
2026-07-27 17:36:16 +0300

    Добавлен bootstrap.py для создания директории data при первом запуске. Добавлено небольшое логирование и log.py как центр

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+import bootstrap
 from src.bronze import bronze_data
+from src.logger import log
 from src.preparing import preparing_orig_file
-import pandas as pd
-
-from src.repository import conn_postgresql, finance_rep
+from src.repository import conn_postgresql
 from src.silver import silver_data
-
 
 def main():
 
-    conn = conn_postgresql.connect()
+    if bootstrap.init():
+        conn = conn_postgresql.connect()
 
-    df_origin = preparing_orig_file.preparing_raw_file()
-    df_bronze = bronze_data.load_data(df_origin, conn)
-    df_silver = silver_data.processing_data_silver(df_bronze, conn)
+        df_origin = preparing_orig_file.preparing_raw_file()
+        df_bronze = bronze_data.load_data(df_origin, conn)
+        df_silver = silver_data.processing_data_silver(df_bronze, conn)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+        log.info("Close connect PostgreSQL DB:finance")
 
 
 if __name__ == '__main__':
--- src/repository/conn_postgresql.py
+++ src/repository/conn_postgresql.py
@@ -2,15 +2,25 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
+from psycopg2._psycopg import OperationalError
+
+from src.logger import log
+
 load_dotenv()
 
 def connect():
-    conn = psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST"),
-        database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        port=os.getenv("POSTGRES_PORT")
-    )
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("POSTGRES_HOST"),
+            database=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            port=os.getenv("POSTGRES_PORT")
+        )
 
-    return conn
+        log.info("Connect PostgreSQL DB:finance")
+
+        return conn
+
+    except OperationalError as e:
+        log.info(f"Error connect PostgreSQL: {e}")
